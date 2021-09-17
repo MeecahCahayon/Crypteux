259c4d53b30808e8e3bee973a8d97412756c22aa
Meecah Cahayon <mc211@students.waikato.ac.nz>
2021-09-17 20:43:16 +1200

    Created template for the Crypteux Website

---
+++ html/assets/js/mainscript.js
@@ -0,0 +1,66 @@
+/************************** FOR AJAX  ***************************/
+/*						JS AJAX OR JQUERY AJAX					*/
+/****************************************************************/
+
+// CREATING ASYNCHRONOUS AJAX REQUEST
+function ajaxRequest(url, method, data, callback, json) {
+
+	let request = new XMLHttpRequest();
+	request.open(method, url, true);
+
+	if (method == "POST") {
+		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	}
+
+	request.onreadystatechange = function() {
+		// IF REQUEST IS READY TO BE PROCCESS
+		if (request.readyState === 4) {
+			// IF REQUEST WAS SUCCESSFUL
+			if (request.status === 200) {
+				// GET RESPONSE AND CALLBACK
+				if (json){
+					response = JSON.parse(request.responseText);
+				}
+				else{
+					response = request.responseText;
+				}
+
+				callback(response);
+			} else {
+				handleError(request.statusText);
+			}
+		}
+	}
+
+	request.send(data);	
+}
+
+function ajaxJquery(url, callback) {
+	$.getJSON(url, callback);
+}
+
+function handleError(error) { console.log(error); }
+
+/****************************************************************/
+/*						  FOR LOGGING OUT						*/
+/****************************************************************/
+
+/****************************************************************/
+/*						  FOR ACTIVE MENU						*/
+/****************************************************************/
+
+// DIRECT USER TO THE PAGE AND MAKE IT THE ACTIVE PAGE
+function gotopage(pageID) {
+
+	// REDIRECT USER TO PAGE
+	switch(pageID) {
+		case "dashpage":
+			window.location.replace("dashpage.html");
+			break;
+		case "favpage":
+			window.location.replace("favpage.html");
+			break;
+	}
+}
+
+

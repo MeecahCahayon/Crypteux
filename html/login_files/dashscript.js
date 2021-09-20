75dfa998a0eb6d060d65d9b85c3ff88bb9a7100f
eunicejtl <nicellobet@gmail.com>
2021-09-21 01:29:57 +1200

    google login and logout

---
+++ html/login_files/dashscript.js
@@ -0,0 +1,168 @@
+/********************* FOR CREATING COIN LIST *******************/
+/*						  										*/
+/****************************************************************/
+const coins = [];
+
+function get_allCoins() {
+
+	$.ajax({
+
+		// PUBLIC API
+		url: 'https://mkuvib9bgi.execute-api.ap-southeast-2.amazonaws.com/pumped-backend-api/coins',
+		// PRIVATE API
+		// url: 'https://zi7y07eh2h-vpc-3c41bf5a.execute-api.ap-southeast-2.amazonaws.com/pumped-backend-api-private/coins',
+		
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		type: "GET",
+		dataType: "json",
+		data: {
+		},
+		success: function (response) {
+			store_allCoins(response);
+		},
+		error: function (response) {
+			error_msg(response);
+		}
+	});
+}
+
+function store_allCoins(response) {	
+
+	// FOR EACH COIN
+	$.each(response, function(resIndex, eachCoin) {
+
+		let coin = {
+
+			coinGeckoID: eachCoin.coinGeckoID,
+			coinName: eachCoin.coinName,
+			Symbol: eachCoin.Symbol
+		};
+
+		coins.push(coin);
+	});
+
+	// // FOR TESTING
+	// for (var i = 0; i < 10; i++) {
+
+	// 	let coin = {
+
+	// 		coinGeckoID: coins[i].coinGeckoID,
+	// 		coinName: coins[i].coinName,
+	// 		Symbol: coins[i].Symbol
+	// 	};
+
+	// 	console.log(coin);
+	// }
+
+	// for (var i = 900; i < 910; i++) {
+
+	// 	let coin = {
+
+	// 		coinGeckoID: coins[i].coinGeckoID,
+	// 		coinName: coins[i].coinName,
+	// 		Symbol: coins[i].Symbol
+	// 	};
+
+	// 	console.log(coin);
+	// }
+
+	// console.log(coins.length);
+}
+
+/*********************** FOR SEARCHING COINS ********************/
+/*						  										*/
+/****************************************************************/
+
+// SEARCH EVERYTIME USER ENTER A CHAR
+$(document).ready(function() {
+	$("#coinSearchInput").keyup(function() {
+
+		// WHEN ENTER KEY IS PRESSED, TRIGGER BUTTON CLICK
+		if(event.key === 'Enter') {
+			seacrhCoin($("#coinSearchInput").val());
+		}
+	});
+});
+
+$(document).ready(function() {
+	$("#coinSearchBtn").click(function() {
+
+		console.log("Button Clicked");
+		get_allCoins(displayCoin);
+	});
+});
+
+function seacrhCoin(coin) {
+	
+	var userInput = coin.trim();
+
+	// CHECK IF THERE'S AN INPUT TO SEARCH
+	if (userInput != "") {
+
+		console.log("Enter Pressed");
+		get_allCoins(displayCoin);
+	}
+}
+
+function displayCoin(response) {
+	
+	console.log(response);
+}
+
+/************************ FOR CLICKING COINS ********************/
+/*						  										*/
+/****************************************************************/
+// IF A COIN IS CLICKED
+$(document).ready(function() {
+	$(".coins").click(function() {
+
+		var selectedCoin = this.firstElementChild;
+		goto_coin(selectedCoin.id, selectedCoin.innerHTML);
+	});
+});
+
+function goto_coin(coinId, coinName) {
+
+	window.location.href = encodeURI("coinpage.html?coinID=" + coinId + "&coinName=" + coinName);
+}
+
+
+/************************ API CALL FUNCTIONS ********************/
+/*						  										*/
+/****************************************************************/
+
+function error_msg(response) {
+	console.log(response);
+}
+
+
+
+
+
+// /*** CREATE EVENTS ***/
+// // SEARCH EVERYTIME USER ENTER A CHAR
+// _widgetUI.searchInput.addEventListener("keyup", function(event) {
+
+// 	// WHEN ENTER KEY IS PRESSED, TRIGGER BUTTON CLICK
+// 	if(event.key === 'Enter') {
+// 		_widgetUI.searchBtn.click();
+// 	}
+
+// 	// CHANGE THE MORE/LESS BUTTON INNERHTML TO 'MORE'
+// 	_widgetUI.moreBtn.innerHTML = _contElemMoreBtn;
+
+// 	// SEARCH THE RECIPE FROM THE DATABASE
+// 	_getSearchRecipe(_widgetUI.searchInput.value);
+// });
+
+// // WHEN SEARCH BUTTON IS PRESS
+// _widgetUI.searchBtn.onclick = function() {
+	
+// 	// CHANGE THE MORE/LESS BUTTON INNERHTML TO 'MORE'
+// 	_widgetUI.moreBtn.innerHTML = _contElemMoreBtn;
+
+// 	// SEARCH THE RECIPE FROM THE DATABASE
+// 	_getSearchRecipe(_widgetUI.searchInput.value);
+// }
--- html/login_files/mainscript.js
+++ html/login_files/mainscript.js
@@ -43,52 +43,9 @@ function ajaxJquery(url, callback) {
 
 function handleError(error) { console.log(error); }
 
-/************************ FOR LOGGING IN ************************/
-/*						  										*/
-/****************************************************************/
-var onLoad = function() {
-	var auth2;
-	gapi.load('auth2', function() {
-		// load gapi lib
-		auth2 = gapi.auth2.init({
-			client_id: '331364600378-n4u9qepoebkc5fdib5jvop86fm44ff68.apps.googleusercontent.com'
-		});
-
-		auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
-	})
-}
-
-// successful login
-var onSuccess = function(user) {
-	console.log('Signed in as ' + user.getBasicProfile().getName());
-	var profile = user.getBasicProfile();
-
-	var myUserEntity = {};
-	myUserEntity.Id = profile.getId();
-	myUserEntity.Name = profile.getName();
-	
-	//Store the entity object in sessionStorage where it will be accessible from all pages of your site.
-	sessionStorage.setItem('myUserEntity',JSON.stringify(myUserEntity));
-	
-	location.href = 'dashpage.html';
-}
-
-// failed login
-var onFailure = function(error) {
-	console.log(error);
-}
-
 /************************ FOR LOGGING OUT ***********************/
 /*						  										*/
 /****************************************************************/
-function logout() {
-	//Don't forget to clear sessionStorage when user logs out
-	sessionStorage.clear()
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-}
 
 /************************ FOR ACTIVE MENU ***********************/
 /*						  										*/
